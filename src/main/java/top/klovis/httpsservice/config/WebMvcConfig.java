7eb16cdc1c6f3953edf2e7bdc53551cbfa1e7bee
zhang ke <zhangke@ainsurtech.com>
2018-02-23 19:04:07 +0800

    增加新保的http redirect https 代码

--- src/main/java/top/klovis/httpsservice/config/WebMvcConfig.java
+++ src/main/java/top/klovis/httpsservice/config/WebMvcConfig.java
@@ -1,9 +1,49 @@
 package top.klovis.httpsservice.config;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
+    private int httpPort = 8081;
+    private int httpsPort = 443;
+    @Bean
+    public EmbeddedServletContainerFactory servletContainerFactory() {
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                //SecurityConstraint必须存在，可以通过其为不同的URL设置不同的重定向策略。
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        factory.addAdditionalTomcatConnectors(createHttpConnector());
+        return factory;
+    }
 
+    private Connector createHttpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        /**
+         *Set the secure connection flag that will be assigned to requests received through this connector.
+         *@param secure The new secure connection flag
+         *if connector.setSecure(true), the http use the http and https use the https;
+         * elseif connector.setSecure(false),the http redirect
+         */
+        connector.setSecure(true);
+        connector.setPort(httpPort);
+        connector.setRedirectPort(httpsPort);
+        return connector;
+    }
 }
